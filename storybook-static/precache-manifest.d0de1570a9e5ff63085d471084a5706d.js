202e2c276fd6d38069ab0d1c2a0e31d8f68316f0
mindbox_dev3 <manojkhatri.designs@gmail.com>
2020-03-28 00:09:56 +0530

    local commit

---
+++ storybook-static/precache-manifest.d0de1570a9e5ff63085d471084a5706d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "405b45bf3087120ef677cdffa10330b4",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d01a576cec582e7daca9.bundle.js"
+  },
+  {
+    "url": "runtime~main.d01a576cec582e7daca9.bundle.js"
+  },
+  {
+    "revision": "2a47669fce861e36fc56",
+    "url": "static/css/main.97cfd8fa.chunk.css"
+  },
+  {
+    "revision": "9434ad87ebe7878b2c1af36011b4504b",
+    "url": "static/media/Checked_Circle_Solid.9434ad87.svg"
+  },
+  {
+    "revision": "08bb7f3d945f68f01fa236cbaefc09e9",
+    "url": "static/media/Delete_Circle_Solid.08bb7f3d.svg"
+  },
+  {
+    "revision": "808ee185733a3197b911c1de159cc8a5",
+    "url": "static/media/avitar.808ee185.png"
+  },
+  {
+    "revision": "6c31352c89edb05ce271664b5b4e82f3",
+    "url": "static/media/downarrow.6c31352c.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e27cae9562b887e588ba456b9a4f65bc",
+    "url": "static/media/mindbox.e27cae95.svg"
+  },
+  {
+    "url": "vendors~main.d01a576cec582e7daca9.bundle.js"
+  },
+  {
+    "url": "vendors~main.d01a576cec582e7daca9.bundle.js.LICENSE.txt"
+  }
+]);
